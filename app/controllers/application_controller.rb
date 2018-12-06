c770dbb6178c45e0aacbaff72d09cb7b5aebad39
github username <github email address>
2018-12-06 18:32:25 -0500

    Vicky's help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def main
+    render layout: 'main'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :zodiacs, only: [:index, :show]
   resources :locations, only: [:index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get '/', to: 'application#main', as: 'main'
 end
