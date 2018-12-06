50026f8b994683443e2dda505f67507076963394
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-12-06 14:34:58 -0500

    adds route and minor styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def main
+    # @users = User.all
+    # @zodiacs = Zodiac.all
+    # @locations = Location.all
+    render :main
+  end
 end
