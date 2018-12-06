c50fc9b538c26f06adf5286b553d0767820492da
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-12-06 18:37:23 -0500

    completing merge for bulma files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def main
-<<<<<<< HEAD
-    # @users = User.all
-    # @zodiacs = Zodiac.all
-    # @locations = Location.all
-    render :main
-=======
     render layout: 'main'
->>>>>>> c770dbb6178c45e0aacbaff72d09cb7b5aebad39
   end
 end
