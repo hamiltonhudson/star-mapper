2dcc68d62eee79f5142263f0b7060ffe4e6fb631
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-12-06 21:06:34 -0500

    added styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :zodiacs, only: [:index, :show]
   resources :locations, only: [:index, :show]
-<<<<<<< HEAD
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/main", to: "application#main"
-=======
-
-get '/', to: 'application#main', as: 'main'
->>>>>>> c770dbb6178c45e0aacbaff72d09cb7b5aebad39
+  get '/', to: 'application#main', as: 'main'
 end
